43a3a8f1ac801a3d8e8230f016e70ecb1f13250f
antim001 <antimsarker0@gmail.com>
2025-04-09 17:42:12 +0600

    routes are added

--- server/userSeed.js
+++ server/userSeed.js
@@ -1,7 +1,9 @@
 import User from './models/User.js'
 import bcrypt from 'bcrypt'
+import connectToDatabase from './db/db.js'
 
 const userRegister =async ()=>{
+    connectToDatabase()
     try{
   const hashPassword =await bcrypt.hash('admin',10)
 
