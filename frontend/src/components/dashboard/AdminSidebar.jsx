149f1160a56e0cc67f57244942af05007b6e630e
antim001 <antimsarker0@gmail.com>
2025-04-30 21:59:25 +0600

    admin sidebar is added

---
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+const AdminSidebar = () => {
+  return (
+    <div>
+      <div>
+        <h3>Employee Ms</h3>
+      </div>
+
+<div>
+    <NavLink/>
+</div>
+    </div>
+  )
+}
+
+export default AdminSidebar
--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useAuth } from '../context/authContext'; // ✅ You need this line
-
+import AdminSidebar from '../components/dashboard/AdminSidebar.jsx'
 function AdminDashboard() {
   const { user ,loading} = useAuth();
   
   return (
     <div>
-      AdminDashboard {user?.name}
+    
     </div>
   )
 }
